3b74dac8555092c2ecf4714586ac100bbc6949d4
내 이름 <you@example.com>
2025-04-20 18:38:22 +0900

    first commit

--- snsProject/settings.py
+++ snsProject/settings.py
@@ -117,6 +117,10 @@ DATABASES = {
         'PASSWORD': '0623',
         'HOST': 'mysql',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode = "STRICT_TRANS_TABLES"',
+            'charset': 'utf8mb4',
+        }
     }
 }
 
