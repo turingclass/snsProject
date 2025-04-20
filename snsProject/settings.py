b4d4f591553d197a5e2c36f075755553a231df03
내 이름 <you@example.com>
2025-04-20 15:33:11 +0900

    first commit

--- snsProject/settings.py
+++ snsProject/settings.py
@@ -100,13 +100,25 @@ WSGI_APPLICATION = 'snsProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'turing',
+        'PASSWORD': '0623',
+        'HOST': 'mysql',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -146,6 +158,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # 아래 부분 추가
 STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 
